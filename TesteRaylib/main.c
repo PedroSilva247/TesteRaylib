934e41fa269a5eb8b8fa3f05fa5a8a72bc41c696
Pedro Silveira da Silva <psilveiradasilva552@gmail.com>
2026-09-05 22:05:09 -0300

    addPlatform and platformArray

--- TesteRaylib/main.c
+++ TesteRaylib/main.c
@@ -5,6 +5,7 @@
 #define GRAVITY 0.3f
 #define WINDOW_HEIGHT 800.0f
 #define WINDOW_WIDTH 1600.0f
+#define MAX_PLATFORMS 20
 
 typedef struct {
     Rectangle top, left, bottom, right;
@@ -24,108 +25,13 @@ typedef struct {
     bool leftTouch;
 } Player;
 
+typedef struct {
+    Rectangle rec;
+    HitBox hitBox;
+    Color color;
+} Platform;
 
 
-Rectangle platforms[] = {
-	{ 0.0f, 700.0f, 400.0f, 100.0f },
-	{ 500.0f, 600.0f, 300.0f, 100.0f },
-	{ 900.0f, 500.0f, 200.0f, 100.0f },
-	{ 1200.0f, 400.0f, 300.0f, 100.0f }
-};
-int platformCount = sizeof(platforms) / sizeof(platforms[0]);
-
-void movePlayer(Player* player, float speed, char axis) {
-    if (axis == 'x') {
-        player->body.x += speed;
-    }
-    else if (axis == 'y') {
-        player->body.y += speed;
-    }
-}
-
-void updateHitBox(Rectangle* rec, HitBox* hitBox) {
-    hitBox->top.x = rec->x;
-    hitBox->top.y = rec->y;
-
-    hitBox->left.x = rec->x;
-    hitBox->left.y = rec->y;
-
-    hitBox->bottom.x = rec->x;
-    hitBox->bottom.y = rec->y + rec->height - 2.0f;
-
-    hitBox->right.x = rec->x + rec->width - 2.0f;
-    hitBox->right.y = rec->y;
-}
-
-void updatePlayer(Player* player) {
-    if (player->speedX != 0) {
-        player->body.x += player->speedX;
-    }
-
-    player->body.y += player->speedY;
-    player->speedY += GRAVITY;
-    updateHitBox(&(player->body), &(player->hitBox));
-
-    /*player->bottom.x = player->body.x + (player->body.width - player->bottom.width) / 2;
-    player->bottom.y = player->body.y + player->body.height;
-
-    player->top.x = player->body.x + (player->body.width - player->top.width) / 2;
-    player->top.y = player->body.y - player->top.height;
-
-    player->right.x = player->body.x + player->body.width;
-    player->right.y = player->body.y + (player->body.height - player->right.height) / 2;
-
-    player->left.x = player->body.x - player->left.width;
-    player->left.y = player->body.y + (player->body.height - player->left.height)/2;*/
-
-}
-
-void updateCollisions(Player* player, Rectangle* ground) {
-     player->onGround = false;
-     player->bottomTouch = false;
-     player->topTouch = false;
-     player->rightTouch = false;
-     player->leftTouch = false;
-
-     if (CheckCollisionRecs(player->hitBox.bottom, *ground)) {
-         player->onGround = true;
-         player->bottomTouch = true;
-         player->body.y = ground->y - player->body.height;
-     }
-
-     for (int i = 0; i < platformCount; i++) {
-         if (CheckCollisionRecs(player->hitBox.bottom, platforms[i])) {
-             player->bottomTouch = true;
-             player->onGround = true;
-             player->body.y = platforms[i].y - player->body.height;
-         }
-         if (CheckCollisionRecs(player->hitBox.top, platforms[i])) {
-             player->topTouch = true;
-             //player->body.y = platforms[i].y + platforms[i].height;
-         }
-         if (CheckCollisionRecs(player->hitBox.right, platforms[i])) {
-             player->rightTouch = true;
-             player->body.x = platforms[i].x - player->body.width;
-         }
-         if (CheckCollisionRecs(player->hitBox.left, platforms[i])) {
-             player->leftTouch = true;
-             player->body.x = platforms[i].x + platforms[i].width;
-         }
-     }
-
-     if (player->bottomTouch) {
-         player->speedY = 0;
-     }
-     if (player->topTouch) {
-         player->speedY = -GRAVITY;
-     }
-     if (player->leftTouch) {
-         player->speedX = 0; 
-     }
-     if (player->rightTouch) {
-         player->speedX = 0;
-     }
-}
 
 HitBox* createRecHitBox(Rectangle* rec) {
     HitBox *p = (HitBox*) malloc(sizeof(HitBox));
@@ -161,13 +67,118 @@ HitBox* createRecHitBox(Rectangle* rec) {
     return p;
 }
 
+Platform* createPlatform(float x, float y, float width, float height, Color color) {
+    Platform* p = (Platform*) malloc(sizeof(Platform));
+    if (p != NULL) {
+        Rectangle rec = { x, y, width, height };
+        *p = (Platform){
+            rec,
+            *createRecHitBox(&rec),
+            color
+        };
+    }
 
+    return p;
+}
 
+void addPlatform(int* countPlatforms, Platform** platformsArray, Platform* platform) {
+    platformsArray[*countPlatforms] = platform;
+    (*countPlatforms)++;
+}
+
+// UPDATES
+
+void updateHitBox(Rectangle* rec, HitBox* hitBox) {
+    hitBox->top.x = rec->x;
+    hitBox->top.y = rec->y;
+
+    hitBox->left.x = rec->x;
+    hitBox->left.y = rec->y;
+
+    hitBox->bottom.x = rec->x;
+    hitBox->bottom.y = rec->y + rec->height - 2.0f;
+
+    hitBox->right.x = rec->x + rec->width - 2.0f;
+    hitBox->right.y = rec->y;
+}
+void updatePlayer(Player* player) {
+    if (player->speedX != 0) {
+        player->body.x += player->speedX;
+    }
+
+    player->body.y += player->speedY;
+    player->speedY += GRAVITY;
+
+    updateHitBox(&(player->body), &(player->hitBox));
+
+}
+void updateCollisions(Player* player, Rectangle* ground) {
+     player->onGround = false;
+     player->bottomTouch = false;
+     player->topTouch = false;
+     player->rightTouch = false;
+     player->leftTouch = false;
+
+     if (CheckCollisionRecs(player->hitBox.bottom, *ground)) {
+         player->onGround = true;
+         player->bottomTouch = true;
+         player->body.y = ground->y - player->body.height;
+     }
+
+     //for (int i = 0; i < platformCount; i++) {
+     //    if (CheckCollisionRecs(player->hitBox.bottom, platforms[i])) {
+     //        player->bottomTouch = true;
+     //        player->onGround = true;
+     //        player->body.y = platforms[i].y - player->body.height;
+     //    }
+     //    if (CheckCollisionRecs(player->hitBox.top, platforms[i])) {
+     //        player->topTouch = true;
+     //        //player->body.y = platforms[i].y + platforms[i].height;
+     //    }
+     //    if (CheckCollisionRecs(player->hitBox.right, platforms[i])) {
+     //        player->rightTouch = true;
+     //        player->body.x = platforms[i].x - player->body.width;
+     //    }
+     //    if (CheckCollisionRecs(player->hitBox.left, platforms[i])) {
+     //        player->leftTouch = true;
+     //        player->body.x = platforms[i].x + platforms[i].width;
+     //    }
+     //}
+
+     if (player->bottomTouch) {
+         player->speedY = 0;
+     }
+     if (player->topTouch) {
+         player->speedY = -GRAVITY;
+     }
+     if (player->leftTouch) {
+         player->speedX = 0; 
+     }
+     if (player->rightTouch) {
+         player->speedX = 0;
+     }
+}
+
+// ACTIONS
+
+void movePlayer(Player* player, float speed, char axis) {
+    if (axis == 'x') {
+        player->body.x += speed;
+    }
+    else if (axis == 'y') {
+        player->body.y += speed;
+    }
+}
 void jumpPlayer(Player* player, float initialSpeed) {
     player->speedY = initialSpeed;
 }
 
+// MAIN
+
 int main() {
+    int countPlatforms = 0;
+    Platform** platforms = (Platform**)malloc(MAX_PLATFORMS * sizeof(Platform*));
+
     InitWindow((int) WINDOW_WIDTH, (int) WINDOW_HEIGHT, "Jogo 01");
 
     SetTargetFPS(60);
@@ -185,27 +196,7 @@ int main() {
 
     player.hitBox = *createRecHitBox(&(player.body));
 
-    /*player.bottom.x = player.body.x + 1.0f;
-    player.bottom.y = player.body.y + player.body.height;
-    player.bottom.width = player.body.width - 2.0f;
-    player.bottom.height = 3.0f;
-
-    player.top.x = player.body.x + 1.0f;
-    player.top.y = player.body.y;
-    player.top.width = player.body.width - 2.0f;
-    player.top.height = 3.0f;
-
-    player.right.x = player.body.x + player.body.width;
-    player.right.y = player.body.y + 1.0f;
-    player.right.width = 3.0f;
-    player.right.height = player.body.height - 2.0f;
-
-    player.left.x = player.body.x;
-    player.left.y = player.body.y + 1.0f;
-    player.left.width = 3.0f;
-    player.left.height = player.body.height - 2.0f;*/
-
-    // RESOLVER O PROBLEMA DE TELEPORTAR EM COLISAO LATERAL / ENTRAR NO CHAO EM COLISAO VERTIVAL DE ALTA VELOCIADE
+    // RESOLVER O PROBLEMA DE TELEPORTAR EM COLISAO LATERAL / ENTRAR NO CHAO EM COLISAO VERTICAL DE ALTA VELOCIADE
 
 	Rectangle ground;
 	ground.height = 100.0f;
@@ -213,6 +204,15 @@ int main() {
 	ground.x = 0.0f;
 	ground.y = WINDOW_HEIGHT - ground.height;
 
+    // Platforms
+
+    Platform platform1 = *createPlatform(100.0f, 650.0f, 200.0f, 40.0f, RED);
+    Platform platform2 = *createPlatform(500.0f, 650.0f, 200.0f, 40.0f, BLUE);
+    Platform platform3 = *createPlatform(900.0f, 650.0f, 200.0f, 40.0f, DARKPURPLE);
+
+    addPlatform(&countPlatforms, platforms, &platform1);
+    addPlatform(&countPlatforms, platforms, &platform2);
+    addPlatform(&countPlatforms, platforms, &platform3);
 
     
 
@@ -248,9 +248,15 @@ int main() {
             ClearBackground(WHITE);
 
             DrawRectangleRec(ground, GREEN);
-            for(int i = 0; i < platformCount; i++) {
-                DrawRectangleRec(platforms[i], BROWN);
+            
+            for (int i = 0; i < countPlatforms; i++) {
+                DrawRectangleRec(platforms[i]->rec, platforms[i]->color);
+                DrawRectangleRec(platforms[i]->hitBox.bottom, BLACK);
+                DrawRectangleRec(platforms[i]->hitBox.top, BLUE);
+                DrawRectangleRec(platforms[i]->hitBox.right, PURPLE);
+                DrawRectangleRec(platforms[i]->hitBox.left, PINK);
             }
+
             DrawRectangleRec(player.body, RED);
 
             DrawRectangleRec(player.hitBox.bottom, BLACK);
